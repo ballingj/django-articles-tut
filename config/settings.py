0906d7d0e48f42c8a112c8d8534484cd1a24b3ca
Jeff Ballinger <jeffrey.ballinger@gmail.com>
2024-12-12 22:48:39 -0600

    prepared for deploy

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-izzjc$%v=9c79b@dl9mjeesl$1jgpglrh02@b2%^y*1j4&b+&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
